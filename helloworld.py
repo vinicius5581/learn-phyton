a18f5269a6b9cd6dee7bef5cbf7d3696c3734757
Vinicius Santana <santanaviny@gmail.com>
2019-01-03 23:35:34 -0800

    basics

---
+++ helloworld.py
@@ -0,0 +1,44 @@
+# comment
+
+# data types
+# numbers
+# strings
+# list
+# tuple
+# dictionary
+
+print("hello world")
+
+print(int(10.0))
+print(float(10))
+print(str(10))
+
+
+hello = "hello world"
+print(hello[0])
+print(hello[0:4])
+print(hello[-1])
+print(hello[1:-1])
+print(hello[-3:-1])
+
+print("he" in "hello")
+print("w" in "hello")
+
+
+lister = [1,2,4, 'hello']
+print(lister)
+lister.append(2)
+print(lister)
+print(lister[0])
+print(lister.index(1))
+print(lister.index('hello'))
+print(lister.count(2))
+lister.remove(2)
+print(lister)
+lister.reverse()
+print(lister)
+
+numberList = [1, 5, 10, 6, 4, 7]
+print(numberList)
+numberList.sort()
+print(numberList)
