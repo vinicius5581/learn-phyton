a1bed87c92095ec2146f2a6d2105bff73bbdeef8
Vinicius Santana <santanaviny@gmail.com>
2019-01-05 01:22:57 -0800

    Pause break and continue

---
+++ pauseBreakContinue.py
@@ -0,0 +1,16 @@
+var = 10
+
+while True:
+    var -= 1
+    if (var == 6):
+        continue
+    print(var)
+    if (var == 0):
+        break
+    
+print("Done while loop")
+
+for x in range(3):
+    pass
+
+print("Done for loop")
